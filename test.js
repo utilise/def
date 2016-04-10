f3e3ee219a60441dc4f585295fb21df858120000
pemrouz <p3pedram@hotmail.com>
2016-04-10 16:59:43 +0100

    allow/reject redefinition via configurable descriptor

--- test.js
+++ test.js
@@ -7,4 +7,19 @@ describe('def', function() {
     expect(def(o, 'a', 5)).to.eql(5)
     expect(o.a).to.eql(5)
   })
+
+  it('should define writable property', function() {
+    var o = {}
+    expect(def(o, 'a', 5, 1)).to.eql(5)
+    o.a = 6
+    expect(o.a).to.eql(6)
+  })
+
+  it('should define configurable property', function() {
+    var o = {}
+    expect(def(o, 'a', 5, 0, 1)).to.eql(5)
+    expect(def(o, 'a', 7, 0, 1)).to.eql(7)
+    expect(o.a).to.eql(7)
+  })
+
 })
